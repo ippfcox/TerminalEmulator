67be62237195e8966974ef0373cf0eaa79c11b17
ippfcox <765472938@qq.com>
2026-08-31 21:17:16 +0800

    feat: remove old hand-crafted code

--- src/main.cpp
+++ src/main.cpp
@@ -1,35 +1,8 @@
-#include <array>
-#include <iostream>
-
-#include "pty/ptyProcess.hpp"
+#include <spdlog/spdlog.h>
 
 int main()
 {
-    terminal::PtyProcess proc;
-
-    if (!proc.startShell())
-    {
-        return 1;
-    }
-
-    proc.write("echo hello\n");
-
-    std::array<std::byte, 4096> buffer;
-
-    while (true)
-    {
-        ssize_t n =
-            proc.read(buffer);
-
-        if (n > 0)
-        {
-            std::cout.write(
-                reinterpret_cast<char *>(buffer.data()),
-                n);
-
-            std::cout.flush();
-        }
-    }
+    SPDLOG_INFO("hello");
 
     return 0;
 }
